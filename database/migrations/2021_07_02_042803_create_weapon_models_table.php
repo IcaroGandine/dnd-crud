2051fbde79a8f018a34d6f3d19c6e3f0e091ffdc
Icaro Weiler <icaro.weiler@codenapp.com>
2021-07-02 02:04:23 -0300

    create weapon model, table and migration

---
+++ database/migrations/2021_07_02_042803_create_weapon_models_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeaponModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weapon', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('damage');
+            $table->integer('weight');
+            $table->string('type');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weapon');
+    }
+}
